15546494367e271d037e48cbfb7d4bf95df28508
abeto56 <monostorir@kkszki.hu>
2024-09-30 10:21:45 +0200

    controller kesz

---
+++ ProductAPI/ProductAPI/Connect.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ProductAPI
+{
+    public class Connect
+    {
+        public MySqlConnection Connection { get; set; }
+        private string ConnectionString;
+
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "Shop";
+            User = "root";
+            Password = "";
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(ConnectionString);
+        }
+        
+    }
+}
---
+++ ProductAPI/ProductAPI/Modell/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductAPI.Modell
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
